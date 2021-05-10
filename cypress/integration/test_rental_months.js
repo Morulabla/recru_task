355a00593b245265cd32f12bb3080612adf760d8
Morulabla <jagoda.wielbacka@gmail.com>
2021-05-10 18:39:48 +0200

    add cucumber plugin

--- cypress/integration/test_rental_months.js
+++ cypress/integration/test_rental_months.js
@@ -4,22 +4,30 @@ describe('Rental duration', () => {
     });
     
     it('should change financing option', () => {
-        cy.get('.financement-tile').first().next().click()
+        cy.get('.financement-tile').eq(1).click({force: true})
     })
 
     it('should change monthly period', () => {
-        cy.get('.financement-tile').first().next().within(() => {
-            cy.get('[data-testid=current-months]').contains(48)
-            cy.get('[data-testid=change-month-decrement-button]').click()
-            cy.get('[data-testid=current-months]').contains(36)
+        cy.get('.financement-tile').eq(1).within(() => {
+            cy.get('[data-testid=current-months]')
+                .should('be.visible')
+                .contains(48)
+                
+            cy.get('[data-testid=change-month-decrement-button]')
+                .should('be.visible')
+                .click({force: true})
+ 
+            cy.get('[data-testid=current-months]')
+                .should('be.visible')
+                .contains(36)
         })
         cy.reload()
     })
 
     it('should change monthly price', () => {
-        cy.get('.financement-tile').first().next().within(() => {
+        cy.get('.financement-tile').eq(1).within(() => {
             cy.get('.financing-option-price>span').contains('159 €')
-            cy.get('[data-testid=change-month-decrement-button]').click()
+            cy.get('[data-testid=change-month-decrement-button]').click({force: true})
             cy.get('.financing-option-price>span').contains('169 €')
         })
     })
